cbf301c77b858e602eefaef052e90fdc08517b31
Matias Rodriguez <matiasrodriguezb@gmail.com>
2024-02-03 17:40:38 -0300

    modularizacion de lista y componente card

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+import { Text, View, Image, StyleSheet, Pressable, TextInput } from "react-native";
+import cartLogo from "./assets/cart.png";
+import RemoveModal from "./src/components/RemoveModal";
+import Lista from "./src/components/lista/Lista";
 
 export default function App() {
+  // useState y useEffect hooks para controlar el estado de la aplicación y el ciclo de vida de un componente
+
+  const [inputValue, setInputValue] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [itemSelected, setItemSelected] = useState(null);
+
+  const handleInputChange = (value) => setInputValue(value);
+  
+
+  const handleModal = (id) => {
+    setModalVisible(true);
+    setItemSelected(id);
+    console.log(id);
+  };
+
+  const addItem = () => {
+    const newItem = {
+      name: inputValue,
+      id: new Date().getTime(),
+    };
+    setCartItems([...cartItems, newItem]);
+   
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!</Text>
+      {/* El StatusBar controla la barra de estado del dispositivo */}
       <StatusBar style="auto" />
+
+      {/* Llamamos al modal para eliminar el producto y le pasamos por props toda la data que necesita */}
+      <RemoveModal
+        modalVisible={modalVisible}
+        cartItems={cartItems}
+        setCartItems={setCartItems}
+        setModalVisible={setModalVisible}
+        itemSelected={itemSelected}
+      />
+      <View style={styles.header}>
+    <Text>CARRITO</Text>
+    <Image style={styles.image} source={cartLogo} />
+  </View>
+      <View style={styles.inputContainer}>
+        {/* additem */}
+        
+        <TextInput
+          onChangeText={handleInputChange}
+          value={inputValue}
+          style={styles.input}
+          placeholder="Ingrese un producto"
+        />
+          <Pressable onPress={addItem}>
+            <Text style={{ fontSize: 40 }}>+</Text>
+          </Pressable>
+
+      </View>
+      <Lista cartItems={cartItems} handleModal={handleModal}></Lista>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#ededed",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 14,
+    paddingTop: Constants.statusBarHeight,
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 8,
+    alignItems: "center",
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  productList: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  product: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 4,
+  },
+  input: {
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    width: "90%",
+  },
+  inputContainer: { flexDirection: "row" },
 });
---
+++ src/components/lista/Lista.js
@@ -0,0 +1,49 @@
+import { FlatList, StyleSheet, Text, View, Pressable } from "react-native"
+import Card from "../card";
+
+
+const Lista = props => {
+  const {cartItems, handleModal} = props;
+
+ 
+
+    return (
+        <View style={styles.productList}>
+        
+        <FlatList
+          data={cartItems}
+          renderItem={({ item }) => (
+            <Card>
+            <View style={{ width: 400, flexDirection: "row" }}>
+              
+              <Text style={styles.product}>{item.name}</Text>
+              <Pressable onPress={() => handleModal(item.id)}>
+                <Text style={{ fontSize: 20 }}>🚮</Text>
+              </Pressable>
+              
+            </View>
+            </Card>
+            
+          )}
+          keyExtractor={(item) => item.id}
+        />
+        </View>
+        ) 
+}             
+
+
+
+const styles = StyleSheet.create({
+    productList: {
+      justifyContent: "center",
+      alignItems: "center",
+      paddingVertical: 10,
+    },
+    product: {
+      fontSize: 16,
+      fontWeight: "bold",
+      padding: 4,
+    },
+  })
+
+  export default Lista;
